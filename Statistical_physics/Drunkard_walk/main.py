42b0d89f16d238fd506419b793a411db7d199b64
Tsar <jakub.stawik@gmail.com>
2019-03-13 00:50:58 +0100

    Finished drunkard problem. It generates linear regression, histogram and plots log of standard deviation on log of steps number with the regression for proof.

--- Statistical_physics/Drunkard_walk/main.py
+++ Statistical_physics/Drunkard_walk/main.py
@@ -1,7 +1,9 @@
 from random import uniform
 from multiprocessing import Pool
-from math import sqrt
-from numpy import logspace
+from time import time
+from numpy import logspace, std, polyfit, log, arange
+import matplotlib.pyplot as plt
+import winsound
 
 
 def drunkard(steps=30000):
@@ -14,17 +16,39 @@ def drunkard(steps=30000):
     return position
 
 
-def generate_report(steps=30000, drunkards=10000):
+def generate_std_report(steps=30000, drunkards=10000):
     pool = Pool()
     args = [steps]*drunkards #we need an iterable of arguments for drunkard function
-    results = pool.map(drunkard, args)
+    data = pool.map(drunkard, args)
     pool.close()
     pool.join()
-    result = 0
-    for i in results:
-        result += i**2
-    result = sqrt(result / drunkards)
-    return drunkards, result
+    return std(data)
 
 
-if __name__ == "__main__":
+def generate_hist(lower=1000, upper=5000, drunkards=50000):
+    pool = Pool()
+    l_result = pool.map(drunkard, [lower]*drunkards)
+    u_result = pool.map(drunkard, [upper]*drunkards)
+    plt.hist(x=l_result, bins=upper, histtype='step')
+    plt.hist(x=u_result, bins=upper, histtype='step')
+    plt.show()
+
+
+if __name__ == '__main__':
+    TS = time()
+    space = logspace(1, 5, 50)
+    results = []
+    for i in space:
+        results.append(generate_std_report(steps=int(i)))
+    print(results)
+    linreg = polyfit(log(space), log(results), 1)
+    print(linreg)
+    plt.scatter(log(space), log(results))
+    plt.plot(log(space), log(space)*linreg[0]+linreg[1])
+    plt.title('σ for N steps')
+    plt.xlabel('log N')
+    plt.ylabel('log σ')
+    plt.show()
+    generate_hist()
+    print("Execution time: "+str(time()-TS))
+    # winsound.Beep(500, 300)
