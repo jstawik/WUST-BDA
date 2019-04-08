cf925c6aece56267e079936aa9aec4dcdb94b837
Tsar <jakub.stawik@gmail.com>
2019-04-09 00:02:46 +0200

    Pascal added

--- Stream_programming/List2/src/PascalTriangleRow.scala
+++ Stream_programming/List2/src/PascalTriangleRow.scala
@@ -1,3 +1,23 @@
-class PascalTriangleRow {
+class PascalTriangleRow(val n: Int) {
+  var row:Array[Int] = init(n)
+  def init(n: Int): Array[Int] ={
+    var ret: Array[Int] = new Array[Int](n)
+    ret(0) = 1
+    for(i <- 1 until n){
+      for(j <- i to 1 by -1){
+        ret(j) = ret(j) + ret(j-1)
+      }
+    }
+    ret
+  }
+  def display(): Unit ={
+    for(i <- row) print(i+" ")
+  }
+}
 
+object TestP{
+  def main(args: Array[String]): Unit = {
+    val triangle = new PascalTriangleRow(10)
+    triangle.display()
+  }
 }
