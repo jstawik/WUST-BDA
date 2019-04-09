bc4159d1c5655403a491a69a46cc6f9f16fceb75
jstawik <jakub.stawik@dxc.com>
2019-04-09 10:01:15 +0200

    File for p&c list 3 created

--- Programming_and_classification/LAB3/main.py
+++ Programming_and_classification/LAB3/main.py
@@ -1,3 +1,14 @@
-def f21(a):
-    for idx, i in enumerate(a):
-        print
+from bitstring import *
+
+
+def f21(a: BitArray):
+    for idx, _ in enumerate(a):
+        a.invert(idx)
+        print(a)
+        a.invert(idx)
+
+
+def f22
+
+if __name__ == '__main__':
+    f21(BitArray('0b0111001'))
