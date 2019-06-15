77a52147b44b9a7c7d23fffacc6d6e99d4856f39
Tsar <jakub.stawik@gmail.com>
2019-06-15 16:36:36 +0200

    Create template for breed/infect

--- Statistical_physics/Ising2/cmake-build-debug/driver_code.py
+++ Statistical_physics/Ising2/cmake-build-debug/driver_code.py
@@ -5,13 +5,13 @@ import time
 
 
 def drive(i):
-    os.system(f'Ising2-10.exe {i}')
+    os.system(f'Ising2.exe {i}')
 
 
 if __name__ == '__main__':
     start_time = time.time()
     temps = [i/100 for i in range(130, 450, 5)]
-    pool = Pool()
+    pool = Pool(14)
     pool.map(drive, temps)
     pool.close()
     pool.join()
