6f878a848187f13f579c94fd0ab93f8d9cafd762
jstawik <jakub.stawik@dxc.com>
2019-12-05 12:59:38 +0100

    2nd list of functional programming

--- Functional_Programming/List2.hs
+++ Functional_Programming/List2.hs
@@ -8,8 +8,8 @@ foldmap :: (a -> b) -> [a] -> [b]
 foldmap f = foldr (\x xs->(f x):xs) []
 
 -- TASK 2
-sum_of_squares :: Num a => [a] -> a
-sum_of_squares = DL.foldl' (\a x -> a + x^2) 0
+sum_of_squares :: Integral a => [a] -> a
+sum_of_squares = DL.foldl' (\a x -> a + x^2) 0 . filter even
 
 -- TASK 3
 -- count_prime :: Num a => [a] -> a
