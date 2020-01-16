b2e86a81e42a5f4c8381ad1c101c8ddd9b6cad0e
Jakub Stawik <jakub.stawik@gmail.com>
2020-01-17 00:42:31 +0100

    Some additionals

---
+++ Spark_Code/MR36.sh
@@ -0,0 +1,1 @@
+spark-submit --driver-java-options "-Xms512m -Xmx8g"  --class MR36 ./target/scala-2.11/spark_code_2.11-0.1.jar
---
+++ Spark_Code/MRGR.sh
@@ -0,0 +1,1 @@
+spark-submit --class MRGraphReversal ./target/scala-2.11/spark_code_2.11-0.1.jar
