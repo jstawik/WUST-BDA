43aa0d05549628a21c77b71c56ee080e3626cf48
Jakub Stawik <jakub.stawik@gmail.com>
2020-01-20 21:03:59 +0100

    Simple usage, simple visualisation

--- Complex_systems_theory_and_practice/Market_percolation.py
+++ Complex_systems_theory_and_practice/Market_percolation.py
@@ -1,16 +1,25 @@
 import igraph as ig
-from random import random
+from random import uniform
 
 # Nodes are required to have the following values:
 # name
 # decision (which takes a value from -1, 0, 1 (decline, undecided, buy))
 
 
+def decision(n: int):
+    if n <= -3:
+        return False
+    elif n <= 3:
+        return (n + 3) * 0.15 > uniform(0, 1)
+    else:
+        return 0.9 > uniform(0,1)
+
+
 def get_lattice(n: int):
     lattice = ig.Graph()
     for i in range(n):
         for j in range(n):
-            lattice.add_vertex(f"{i},{j}")
+            lattice.add_vertex(name=f"{i},{j}", decision=0)
     for i in range(n-1):
         for j in range(n-1):
             lattice.add_edge(f"{i},{j}", f"{i+1},{j}")
@@ -21,6 +30,13 @@ def get_lattice(n: int):
     return lattice
 
 
+def print_lattice(g: ig.Graph, n: int):
+    for i in range(n):
+        for j in range(n):
+            print(g.vs.find(f'{i},{j}')['decision'], end=' ')
+        print('')
+
+
 def steps(prev_n: [ig.Vertex]):
     if not prev_n:
         pass
@@ -28,11 +44,16 @@ def steps(prev_n: [ig.Vertex]):
         next_n = set()
         for n in prev_n:
             neighbours = n.neighbors()
-            # TODO: here we use the above to actually flip the state.
-            if (sum(map(lambda x: x['decision'], neighbours))+4 * 0.12) > random.uniform(0, 1):
-                # kinda scales the chance from 0 to 86%
-                n['decision'] = -1
-            else:
+            if decision(sum(map(lambda x: x['decision'], neighbours))):
                 n['decision'] = 1
+            else:
+                n['decision'] = -1
             next_n.update(set(filter(lambda x: x['decision'] == 0, neighbours)))
         steps(next_n)
+
+
+n = 10
+network = get_lattice(n)
+print_lattice(network, n)
+steps([network.vs.find(f'{3},{3}')])
+print_lattice(network, n)
