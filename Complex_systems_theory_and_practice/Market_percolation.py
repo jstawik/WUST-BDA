87c193eed58715ddfd0a8969e9b5c0423625fead
Jakub Stawik <jakub.stawik@gmail.com>
2020-01-22 00:34:06 +0100

    Futher development and pointers

--- Complex_systems_theory_and_practice/Market_percolation.py
+++ Complex_systems_theory_and_practice/Market_percolation.py
@@ -1,61 +1,80 @@
 import igraph as ig
 from random import uniform
+import seaborn as sns
 
 # Nodes are required to have the following values:
 # name
-# decision (which takes a value from -1, 0, 1 (decline, undecided, buy))
+# decision - takes a value from [-1, 0, 1] (decline, undecided, buy))
+# qexpectation - float between 0 and 1
 
 
-def decision(n: int):
-    if n <= -1:
-        return False
-    elif n < 3:
-        return (n + 1) * 0.3 > uniform(0, 1)
-    else:
-        return 0.9 > uniform(0, 1)
+class Model:
+    # for lattice:
+    def __init__(self, n: int):
+        self.n = n
+        self.g = self.get_lattice()
+
+    def decision(self, Q):
+        if self.n <= -2:
+            return False
+        elif self.n < 2:
+            return (self.n + 2)* 0.22 > uniform(0, 1)
+        else:
+            return 0.88 > uniform(0, 1)
+
+    def get_lattice(self):
+        lattice = ig.Graph()
+        for i in range(self.n):
+            for j in range(self.n):
+                lattice.add_vertex(name=f"{i},{j}"
+                                   , decision=0
+                                   , qexpectation=uniform(0, 1))
+        for i in range(self.n-1):
+            for j in range(self.n-1):
+                lattice.add_edge(f"{i},{j}", f"{i+1},{j}")
+                lattice.add_edge(f"{i},{j}", f"{i},{j+1}")
+        for i in range(self.n-1):
+            lattice.add_edge(f"{i},{self.n - 1}", f"{i + 1},{self.n - 1}")
+            lattice.add_edge(f"{self.n - 1},{i}", f"{self.n - 1},{i + 1}")
+        return lattice
+
+    def lattice_to_array (self):
+        array = []
+        for i in range(self.n):
+            row = []
+            for j in range(self.n):
+                row.append(self.g.vs.find(f'{i},{j}')['decision'])
+            array.append(row)
+        return array
+
+    def print_lattice(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print(self.g.vs.find(f'{i},{j}')['decision'], end=' ')
+            print('')
+
+    def steps(self, prev_n: [ig.Vertex], q: float):
+        if not prev_n:
+            pass
+        else:
+            next_n = set()
+            for n in prev_n:
+                neighbours = n.neighbors()
+                if self.decision(sum(map(lambda x: x['decision'], neighbours, q))):
+                    n['decision'] = 1
+                else:
+                    n['decision'] = -1
+                next_n.update(set(filter(lambda x: x['decision'] == 0, neighbours)))
+            next_n = list((filter(lambda x: x['decision'] == 0, next_n)))
+            self.steps(next_n, q)
 
 
-def get_lattice(n: int):
-    lattice = ig.Graph()
-    for i in range(n):
-        for j in range(n):
-            lattice.add_vertex(name=f"{i},{j}", decision=0)
-    for i in range(n-1):
-        for j in range(n-1):
-            lattice.add_edge(f"{i},{j}", f"{i+1},{j}")
-            lattice.add_edge(f"{i},{j}", f"{i},{j+1}")
-    for i in range(n-1):
-        lattice.add_edge(f"{i},{n - 1}", f"{i + 1},{n - 1}")
-        lattice.add_edge(f"{n - 1},{i}", f"{n - 1},{i + 1}")
-    return lattice
-
-
-def print_lattice(g: ig.Graph, n: int):
-    for i in range(n):
-        for j in range(n):
-            print(g.vs.find(f'{i},{j}')['decision'], end=' ')
-        print('')
-
-
-def steps(prev_n: [ig.Vertex]):
-    if not prev_n:
-        pass
-    else:
-        next_n = set()
-        for n in prev_n:
-            neighbours = n.neighbors()
-            if decision(sum(map(lambda x: x['decision'], neighbours))):
-                n['decision'] = 1
-            else:
-                n['decision'] = -1
-            next_n.update(set(filter(lambda x: x['decision'] == 0, neighbours)))
-        next_n = list((filter(lambda x: x['decision'] == 0, next_n)))
-        steps(next_n)
-
-
-n = 10
-network = get_lattice(n)
-print_lattice(network, n)
-network.vs.find(f'{3},{3}')['decision'] = 1
-steps(network.vs.find(f'{3},{3}').neighbors())
-print_lattice(network, n)
+if __name__ == '__main__':
+    n = 100
+    network = Model(n)
+    sx = int(n/2)
+    sy = 0  # int(n/2)
+    # print_lattice(network, n)
+    network.g.vs.find(f'{sx},{int(sy)}')['decision'] = 1
+    network.steps(network.g.vs.find(f'{sx},{sy}').neighbors(), .7)
+    x = sns.heatmap(network.lattice_to_array())
